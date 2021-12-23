0b84907bf5414b44a66925ff12d4cc415ff669a6
André Kuratomi <andrekuratomi@gmail.com>
2021-12-23 18:16:54 -0300

    fix(components/listsearch): listAlbuns and listartists deleted and all lists put in one only comp
    listAlbuns and listartists deleted and all lists put in one only component: ListSearch

---
+++ desafio-manipulae/src/components/ListSearch/index.jsx
@@ -0,0 +1,23 @@
+const ListSearch = () => {
+  return music ? (
+    <ul>
+      {musics.map((music, index) => {
+        <li key={index}>{music}</li>;
+      })}
+    </ul>
+  ) : artists ? (
+    <ul>
+      {artists.map((artist, index) => (
+        <li key={index}>{artist}</li>
+      ))}
+    </ul>
+  ) : (
+    <ul>
+      {albuns.map((album, index) => (
+        <li key={index}>{album}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListSearch;
