d5f5a0d4e28665f6373e6a690b678bfe89786941
André Kuratomi <andrekuratomi@gmail.com>
2021-12-27 20:13:54 -0300

    style(cards styling drafted): n
    n

--- desafio-manipulae/src/store/modules/musics/thunks.js
+++ desafio-manipulae/src/store/modules/musics/thunks.js
@@ -7,8 +7,6 @@ export const findMusicThunk = (input, setError) => (dispatch) => {
   api
     .get(`/search?q=track: "${input}"`)
     .then((response) => {
-      console.log(input);
-      console.log(response);
       const arr = response.data;
       dispatch(findMusic(arr.map((elt) => <Cards elt={elt} type="musics" />)));
       setError(false);
