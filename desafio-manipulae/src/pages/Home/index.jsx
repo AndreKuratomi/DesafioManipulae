e290755cc3cffed7120e16bd1c46dbdfeb2e4a0e
André Kuratomi <andrekuratomi@gmail.com>
2021-12-27 22:09:45 -0300

    feat(home/ & listsearch/): button for changing to page favorites added and lines commented
    button for changing to page favorites added on page Home and lines commented on component listSearch

--- desafio-manipulae/src/pages/Home/index.jsx
+++ desafio-manipulae/src/pages/Home/index.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 import SearchForm from "../../components/SearchForm";
 import ListSearch from "../../components/ListSearch";
 
 const Home = () => {
+  const nav = useNavigate();
+
   return (
     <>
       <h1>Deezer</h1>
       <SearchForm />
       <ListSearch />
+      <button onClick={() => nav.push("/favoritos")}>Ir para Favoritos</button>
     </>
   );
 };
